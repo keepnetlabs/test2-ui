10fc3502fee3ecdd610606ac7f5dfc4e7a495f1c
oalkandev <oalkandev@gmail.com>
2020-06-07 01:29:19 +0300

    storybook added

---
+++ src/stories/index.stories.js
@@ -0,0 +1,20 @@
+import { action } from '@storybook/addon-actions'
+import { linkTo } from '@storybook/addon-links'
+
+import MyButton from '../components/MyButton.vue'
+
+export default {
+  component: MyButton,
+  title: 'Button'
+}
+
+export const withText = () => ({
+  components: { MyButton },
+  template: '<my-button @click="action">Hello Button</my-button>',
+  methods: { action: action('clicked') }
+})
+
+export const withSomeEmoji = () => ({
+  components: { MyButton },
+  template: '<my-button>😀 😎 👍 💯</my-button>'
+})
