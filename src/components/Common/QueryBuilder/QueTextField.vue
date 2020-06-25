5b478abe2b4fa5476739336138c065253c331566
oalkandev <oalkandev@gmail.com>
2020-06-25 16:24:28 +0300

    commit for merge

---
+++ src/components/Common/QueryBuilder/QueTextField.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-text-field label="Outlined" placeholder="Placeholder" outlined></v-text-field>
+</template>
+
+<script>
+export default {
+  name: 'QueTextField'
+}
+</script>
+
+<style scoped></style>
--- src/views/Workshop.vue
+++ src/views/Workshop.vue
@@ -2,6 +2,28 @@
   <section class="workshop">
     <v-container>
       <v-row align="center">
+        <v-col class="d-flex" cols="12">
+          <query-builder :config="config" v-model="query">
+            <template #groupOperator="props">
+              <group-operator-slot :group-operator="props" />
+            </template>
+
+            <template #groupControl="props">
+              <group-ctrl-slot :group-ctrl="props" />
+            </template>
+
+            <template #rule="props">
+              <rule-slot :rules="config.rules" :ruleCtrl="props" />
+            </template>
+          </query-builder>
+        </v-col>
+      </v-row>
+    </v-container>
+  </section>
+  <!--
+  <section class="workshop">
+    <v-container>
+      <v-row align="center">
         <v-col class="d-flex" cols="12" sm="6">
           <v-select :items="items" :menu-props="{ offsetY: true }" outlined></v-select>
         </v-col>
@@ -19,15 +41,48 @@
         </v-col>
       </v-row>
     </v-container>
-  </section>
+  </section>-->
 </template>
 
 <script>
+import QueryBuilder from 'query-builder-vue'
+import GroupOperatorSlot from '../components/Common/QueryBuilder/GroupOperatorSlot'
+import GroupCtrlSlot from '../components/Common/QueryBuilder/GroupCtrlSlot'
+import RuleSlot from '../components/Common/QueryBuilder/RuleSlot'
+import VTextField from 'vuetify/lib/components/VTextField/VTextField'
 export default {
   name: 'Workshop',
+  components: { QueryBuilder, GroupOperatorSlot, GroupCtrlSlot, RuleSlot },
   props: {},
   data: () => ({
-    items: ['Foo', 'Bar', 'Fizz', 'Buzz']
+    query: null,
+
+    config: {
+      operators: [
+        {
+          name: 'AND',
+          identifier: 'AND'
+        },
+        {
+          name: 'OR',
+          identifier: 'OR'
+        }
+      ],
+      rules: [
+        {
+          identifier: 'txt',
+          name: 'Text Selection',
+          component: VTextField,
+          initialValue: ''
+        },
+        {
+          identifier: 'num',
+          name: 'Number Selection',
+          component: VTextField,
+          initialValue: 10
+        }
+      ]
+    }
   }),
   computed: {},
   created: () => {},
