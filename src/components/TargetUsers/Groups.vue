fa111f3e9de53951b8b6430532ea0eb9d4f3e75c
oalkandev <oalkandev@gmail.com>
2020-08-06 15:04:37 +0300

    Label text edited

--- src/components/TargetUsers/Groups.vue
+++ src/components/TargetUsers/Groups.vue
@@ -63,6 +63,7 @@ import DeleteGroupModal from './DeleteGroupModal'
 import {
   COMMON_CONSTANTS,
   getStoreValue,
+  LABEL_STORE,
   PROPERTY_STORE
 } from '../../model/constants/commonConstants'
 import { required } from '../../utils/validations'
@@ -145,7 +146,7 @@ export default {
           download: true
         },
         iEmpty: {
-          message: 'You do not have any groups, yet',
+          message: LABEL_STORE.NO_TARGET_GROUPS_DEFINED,
           subMes: 'Start now',
           btn: 'ADD A GROUP',
           icon: 'mdi-plus'
