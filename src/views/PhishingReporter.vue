8fee618584afb6ee08d1fc26e650f53f4f96c118
Gurkan Ugurlu <gurkan.ugurlu@keepnetlabs.com>
2020-08-07 14:43:42 +0300

    header fix

--- src/views/PhishingReporter.vue
+++ src/views/PhishingReporter.vue
@@ -8,26 +8,6 @@
               <div class="phishing-reporter__stats-card-left">
                 <div
                   class="phishing-reporter__stats-card-left-icon"
-                  style="background-color: #00bcd4;"
-                >
-                  <v-icon color="white" left medium>mdi-account</v-icon>
-                </div>
-              </div>
-              <div class="phishing-reporter__stats-card-right">
-                <h3 class="phishing-reporter__stats-card-right-title" style="color: #00bcd4;">
-                  {{ (phishingReportSummary && phishingReportSummary.onlineUsersCount) || 0 }}
-                </h3>
-                <p class="phishing-reporter__stats-card-right-stats">
-                  Users Online
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="phishing-reporter__stats-cards">
-            <div class="phishing-reporter__stats-card">
-              <div class="phishing-reporter__stats-card-left">
-                <div
-                  class="phishing-reporter__stats-card-left-icon"
                   style="background-color: #2196f3;"
                 >
                   <v-icon color="white" left medium>mdi-puzzle</v-icon>
@@ -39,6 +19,26 @@
                 </h3>
                 <p class="phishing-reporter__stats-card-right-stats">
                   Users have the add-in
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="phishing-reporter__stats-cards">
+            <div class="phishing-reporter__stats-card">
+              <div class="phishing-reporter__stats-card-left">
+                <div
+                  class="phishing-reporter__stats-card-left-icon"
+                  style="background-color: #00bcd4;"
+                >
+                  <v-icon color="white" left medium>mdi-account</v-icon>
+                </div>
+              </div>
+              <div class="phishing-reporter__stats-card-right">
+                <h3 class="phishing-reporter__stats-card-right-title" style="color: #00bcd4;">
+                  {{ (phishingReportSummary && phishingReportSummary.onlineUsersCount) || 0 }}
+                </h3>
+                <p class="phishing-reporter__stats-card-right-stats">
+                  Users Online
                 </p>
               </div>
             </div>
