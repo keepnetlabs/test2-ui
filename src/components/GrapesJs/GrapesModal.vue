8f178d7374ee094bc79b1b149185c79754a21a0c
Arda DURA <arda.dura@gmail.com>
2020-08-18 11:37:12 +0300

    share fix and new grapes modal

--- src/components/GrapesJs/GrapesModal.vue
+++ src/components/GrapesJs/GrapesModal.vue
@@ -12,124 +12,18 @@
 
 <script>
 import grapesjs from 'grapesjs'
-
+import 'grapesjs-blocks-basic'
+import 'grapesjs-preset-newsletter'
 export default {
   name: 'GrapesModal',
   components: grapesjs,
   mounted() {
-    const editorModal = grapesjs.init({
-      // Indicate where to init the editor. You can also pass an HTMLElement
+    const editor = grapesjs.init({
       container: '#gjsModal',
-      // Get the content for the canvas directly from the element
-      // As an alternative we could use: `components: '<h1>Hello World Component!</h1>'`,
-      fromElement: false,
-      // Size of the editor
-      height: '300px',
-      width: 'auto',
-      // Disable the storage manager for the moment
-      storageManager: false,
-      // Avoid any default panel
-      panels: { defaults: [] },
-      blockManager: {
-        appendTo: '#blocksModal',
-        blocks: [
-          {
-            id: 'section', // id is mandatory
-            label: '<b>Section</b>', // You can use HTML/SVG inside labels
-            attributes: { class: 'gjs-block-section' },
-            content: `<section>
-          <h1>This is a simple title</h1>
-          <div>This is just a Lorem text: Lorem ipsum dolor sit amet</div>
-        </section>`
-          },
-          {
-            id: 'text',
-            label: 'Text',
-            content: '<div data-gjs-type="text">Insert your text here</div>'
-          },
-          {
-            id: 'image',
-            label: 'Image',
-            // Select the component once it's dropped
-            select: true,
-            // You can pass components as a JSON instead of a simple HTML string,
-            // in this case we also use a defined component type `image`
-            content: { type: 'image' },
-            // This triggers `active` event on dropped components and the `image`
-            // reacts by opening the AssetManager
-            activate: true
-          }
-        ]
-      }
+      fromElement: 1,
+      storageManager: { type: 0 },
+      plugins: ['gjs-blocks-basic', 'gjs-preset-newsletter']
     })
-    editorModal.BlockManager.add('my-block-id', {
-      label: 'asd',
-      category: 'fff',
-      content: {
-        tagName: 'div',
-        draggable: true,
-        attributes: { 'some-attribute': 'some-value' },
-        components: [
-          {
-            tagName: 'span',
-            content: '<b>Content burya span ici</b>'
-          },
-          {
-            tagName: 'div',
-            // use `content` for static strings, `components` string will be parsed
-            // and transformed in Components
-            components: '<span>Buraya ise component</span>'
-          }
-        ]
-      }
-    })
-    editorModal.Panels.addPanel({
-      id: 'panel-top-modal',
-      el: '.panel__top-modal'
-    })
-    editorModal.Panels.addPanel({
-      id: 'basic-actions-modal',
-      el: '.panel__basic-actions-modal',
-      buttons: [
-        {
-          id: 'visibility',
-          active: true, // active by default
-          className: 'btn-toggle-borders',
-          label: '<u>B</u>',
-          command: 'sw-visibility' // Built-in command
-        },
-        {
-          id: 'export',
-          className: 'btn-open-export',
-          label: 'Exp',
-          command: 'export-template',
-          context: 'export-template' // For grouping context of buttons from the same panel
-        },
-        {
-          id: 'show-json',
-          className: 'btn-show-json',
-          label: 'JSON',
-          context: 'show-json',
-          command(editorModal) {
-            editorModal.Modal.setTitle('Components JSON')
-              .setContent(
-                `<textarea style="width:100%; height: 250px;">
-            ${JSON.stringify(editorModal.getComponents())}
-          </textarea>`
-              )
-              .open()
-          }
-        }
-      ]
-    })
-    editorModal.on('run:export-template:before', (opts) => {
-      console.log('Before the command run')
-      if (0 /* some condition */) {
-        opts.abort = 1
-      }
-    })
-    editorModal.on('run:export-template', () => console.log('After the command run'))
-    editorModal.on('abort:export-template', () => console.log('Command aborted'))
   }
 }
 </script>
