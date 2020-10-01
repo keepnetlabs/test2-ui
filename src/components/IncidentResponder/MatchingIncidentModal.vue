06d84f1d18d117ad5ee4f92e56e774c2ea00adb3
Gurkan Ugurlu <gurkan.ugurlu@keepnetlabs.com>
2020-10-01 10:28:18 +0300

    #10057 finished

---
+++ src/components/IncidentResponder/MatchingIncidentModal.vue
@@ -0,0 +1,134 @@
+<template>
+  <app-dialog
+    :status="status"
+    icon="mdi-email"
+    title="Matching Incidents"
+    :subtitle="getSelectedMatchingIncidentsSubtitle"
+    @changeStatus="closeOverlay"
+    size="maximum"
+    class-name="matching-modal"
+  >
+    <template v-slot:app-dialog-body>
+      <v-card light>
+        <v-list-item class="matching-modal__list-item">
+          <v-list-item-content>
+            <data-table
+              :refName="'matchingInvestigation'"
+              ref="refMatchingInvestigation"
+              :table="tableData"
+              :columns="columns"
+              :countRow="5"
+              :pageSizes="[5, 10, 20, 50, 100]"
+              :showHeader="true"
+              :defaultSort="'subject'"
+              :selectable="false"
+              :filterable="true"
+              :options="true"
+              :rowActions="[]"
+              :cell-padding="15"
+              :empty="empty"
+            />
+          </v-list-item-content>
+        </v-list-item>
+      </v-card>
+    </template>
+    <template v-slot:app-dialog-footer>
+      <div class="d-flex justify-end">
+        <v-btn class="pa-0 k-dialog__button" text color="#2196f3" @click="closeOverlay"
+          >CLOSE
+        </v-btn>
+      </div>
+    </template>
+  </app-dialog>
+</template>
+
+<script>
+import AppDialog from '@/components/AppDialog'
+import DataTable from '@/components/DataTable'
+import { getStoreValue } from '@/model/constants/commonConstants'
+import { getMatchingIncidents } from '@/api/incidentResponder'
+export default {
+  name: 'MatchingIncidentModal',
+  components: {
+    AppDialog,
+    DataTable
+  },
+  props: {
+    status: {
+      type: Boolean
+    },
+    selectedMatch: {
+      type: Object
+    }
+  },
+  data() {
+    return {
+      columns: [
+        {
+          property: 'subject',
+          align: 'left',
+          editable: false,
+          label: 'Subject',
+          fixed: false,
+          sortable: false,
+          show: true,
+          type: 'text',
+          minWidth: '33'
+        },
+        {
+          property: 'createDate',
+          align: 'left',
+          editable: false,
+          label: getStoreValue('createDate'),
+          fixed: false,
+          sortable: false,
+          show: true,
+          type: 'text',
+          minWidth: '33'
+        },
+        {
+          property: 'reportedBy',
+          align: 'left',
+          editable: false,
+          label: getStoreValue('reportedBy'),
+          fixed: false,
+          sortable: false,
+          show: true,
+          type: 'text',
+          minWidth: '34'
+        }
+      ],
+      empty: { message: "There isn't any matching Incidents, yet", btn: '', icon: 'mdi-plus' },
+      tableData: []
+    }
+  },
+  computed: {
+    getSelectedMatchingIncidentsSubtitle() {
+      return this.selectedMatch && `Incidents matching Rule: ${this.selectedMatch['ruleName']}`
+    }
+  },
+  methods: {
+    closeOverlay() {
+      this.$emit('closeOverlay')
+    },
+    callForMatchingIncident() {
+      const payload = {
+        pageNumber: 1,
+        pageSize: 500,
+        orderBy: 'CreateDate',
+        ascending: true
+      }
+      getMatchingIncidents(payload, this.selectedMatch.resourceId).then((response) => {
+        this.$refs.refMatchingInvestigation.loadWithDataArray(response.data.data.results || [])
+      })
+    }
+  },
+  created() {
+    if (this.selectedMatch) {
+      this.callForMatchingIncident()
+    }
+  }
+}
+</script>
+
+<style lang="scss"></style>
