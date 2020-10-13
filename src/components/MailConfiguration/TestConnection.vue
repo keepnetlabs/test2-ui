0d3edb51678cce20f44757cbc080e74f3cd4a845
Arda DURA <arda.dura@gmail.com>
2020-10-13 09:59:00 +0300

    test connection

---
+++ src/components/MailConfiguration/TestConnection.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="test-connection">
+    <v-btn
+      @click="handleTestConnection"
+      outlined
+      rounded
+      medium
+      color="#2196f3"
+      class="ldap-info__btn"
+      >Test Connection
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestConnection',
+  methods: {
+    handleTestConnection() {}
+  }
+}
+</script>
+
+<style scoped></style>
