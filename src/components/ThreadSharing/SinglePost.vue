6af3b77f93353953f9a189e9ae6ddabf6728b694
Gurkan Ugurlu <gurkan.ugurlu@keepnetlabs.com>
2020-10-23 12:09:17 +0300

    #10584 #10590 finished

--- src/components/ThreadSharing/SinglePost.vue
+++ src/components/ThreadSharing/SinglePost.vue
@@ -1004,14 +1004,16 @@ Vue.customElement('k-shadow-frame', KShadowFrame, {
 [data-title]:after {
      content: attr(data-title);
     position: absolute;
-    padding: 8px 16px 8px 16px;
+    padding: 4px 8px;
     bottom: -40px;
     left: 0;
     white-space: nowrap;
     opacity: 0;
     z-index: 99999;
     visibility: hidden;
-    border-radius: 4px;
+  border-radius: 4px;
+    line-height: 1.33;
+    min-height: 24px;
     background: #6d6d6d !important;
     color: rgba(255, 255, 255, 0.87) !important;
     font-family: "Open Sans", sans-serif !important;
