27d293af6c4f101e5caaef6b1ddb11a3bb709783
Arda DURA <arda.dura@gmail.com>
2020-11-12 23:33:24 +0300

    #11368

--- src/components/ThreadSharing/SinglePost.vue
+++ src/components/ThreadSharing/SinglePost.vue
@@ -546,16 +546,16 @@
                 <v-icon :class="{ 'active-act': postDetails && postDetails.isLikedByUser }"
                   >mdi-thumb-up</v-icon
                 >
-                Useful {{ (postDetails && postDetails.likeCount) || post.likeCount }}
+                Useful ( {{ (postDetails && postDetails.likeCount) || post.likeCount }} )
               </v-btn>
               <v-btn
                 v-else-if="postDetails && postDetails.isLikedByUser"
                 @click="userUnlikePost(post.communityPostResourceId, post.communityResourceId)"
-                color="#2196f3"
+                :class="{ 'active-act': postDetails.isLikedByUser }"
                 :id="'unlike-btn' + post.communityPostResourceId"
               >
                 <v-icon class="active-act">mdi-thumb-up</v-icon>
-                Useful {{ (postDetails && postDetails.likeCount) || post.likeCount }}
+                Useful ({{ (postDetails && postDetails.likeCount) || post.likeCount }})
               </v-btn>
               <v-btn
                 :id="'comments-btn' + post.communityPostResourceId"
