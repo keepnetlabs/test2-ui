c727c79def99d0a4738feec0421c47802c0f8270
Arda DURA <arda.dura@gmail.com>
2020-11-17 17:21:43 +0300

    #10610

--- src/components/SwitchAccount.vue
+++ src/components/SwitchAccount.vue
@@ -143,18 +143,7 @@ export default {
     onClickSelectedAccount(account) {
       this.getSelectedCompanyDetails(account)
       this.setDialogBar(false)
-      this.selectCompany(account).then((response) => {
-        /*localStorage.setItem('companyManager', account.companyName)
-        localStorage.setItem('companyId', account.companyResourceId)
-        localStorage.setItem('companyRequestId', account.companyResourceId)
-        localStorage.setItem('companyResourceId', account.companyResourceId)
-        localStorage.setItem('companyName', account.companyName)
-        localStorage.setItem('userId', account.companyResourceId)
-        localStorage.setItem('businessCatId', account.userCompany.licenseTypeResourceId)
-        localStorage.setItem('userName', account.fullName)*/
-        //dispatch('dashboard/selectCompany', account, { root: true })
-        //commit('SET_CURRENTUSER', account)
-      })
+      this.selectCompany(account).then((response) => {})
 
       this.search = ''
     }
