7ae871fa6a3cbc73f2aee91e14fee12814e914b8
Atakan OZGUN <atakan@keepnetlabs.com>
2020-11-18 11:59:48 +0000

    Updated vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,6 @@ const path = require('path')
 
 module.exports = {
   publicPath: '/',
-  productionSourceMap: process.env.NODE_ENV != 'production',
   configureWebpack: {
     module: {
       rules: [
