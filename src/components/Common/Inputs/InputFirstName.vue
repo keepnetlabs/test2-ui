1b9fe56dfe08c0c1085b923f9fb4cc3956841546
Onur Alkan <31579802+oalkandev@users.noreply.github.com>
2020-11-20 05:20:06 +0300

    crlf fixes

--- src/components/Common/Inputs/InputFirstName.vue
+++ src/components/Common/Inputs/InputFirstName.vue
@@ -25,6 +25,7 @@ export default {
       default: 'off'
     },
     rules: {
+      //
       default: () => [
         (v) => Validations.required(v, 'Required'),
         (v) => Validations.startsWithEmpty(v, 'Cannot start with space'),
