5972e7c6da6aa38d3f3f0aad14de6c5e92d0f355
Gurkan Ugurlu <gurkan.ugurlu@keepnetlabs.com>
2020-12-01 17:55:39 +0300

    safari cluster adjustments

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1532,12 +1532,16 @@ export default {
         cell.querySelector('.datatable-chart__empty') ||
         cell.querySelector('.datatable-progress') ||
         cell.querySelector('div')
-
       if ([...span.classList].some((item) => item === 'cell')) {
         span = span.querySelector('div')
       }
-
-      let spanWidth = span.getBoundingClientRect().width + 20 + this.cellPadding
+      let aggregation = 20
+      if (window.safari || navigator.vendor.match(/apple/i)) {
+        if ([...cell.parentNode.classList].some((item) => item === 'el-table__row--level-1')) {
+          aggregation = 0
+        }
+      }
+      let spanWidth = span.getBoundingClientRect().width + aggregation + this.cellPadding
       const padding = getComputedStyle(cell).paddingLeft.slice(0, -2)
       if (![...cell.classList].some((item) => item === 'el-table-column--selection')) {
         if (padding) {
