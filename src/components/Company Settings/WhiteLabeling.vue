f78b3a44a96591c8fadd2dcbfafd8acc9727bcd2
Gurkan Ugurlu <gurkan.ugurlu@keepnetlabs.com>
2021-03-30 15:21:56 +0300

    permissions added

--- src/components/Company Settings/WhiteLabeling.vue
+++ src/components/Company Settings/WhiteLabeling.vue
@@ -224,7 +224,7 @@
           class="white--text btn-util btn-save-changes mb-6"
           color="#2196f3"
           rounded
-          :disabled="isActionButtonDisabled"
+          :disabled="getActionButtonDisabled"
           :style="formValues.isShowReleaseNotes ? { marginTop: '21px' } : { marginTop: '23px' }"
           @click="submit"
         >
@@ -254,6 +254,12 @@ export default {
     InputEmail,
     KFileUpload,
     FormGroup
+  },
+  props: {
+    PERMISSIONS: {
+      type: Object,
+      required: true
+    }
   },
   data() {
     return {
@@ -288,6 +294,9 @@ export default {
     isWhiteLabelLoading() {
       return this.$store.state['whitelabel'].loading
     },
+    getActionButtonDisabled() {
+      return this.isActionButtonDisabled || !this.PERMISSIONS['UPDATE'].hasPermission
+    },
     getMainLogo() {
       return this.formValues.mainLogoFile || this.formValues.mainLogoUrl
     },
@@ -307,7 +316,10 @@ export default {
     }
   },
   created() {
-    this.loadDatas(this.$store.state.whitelabel.loading)
+    const { GET } = this.PERMISSIONS
+    if (GET.hasPermission) {
+      this.loadDatas(this.$store.state.whitelabel.loading)
+    }
   },
   methods: {
     getFileUploadClasses(url = '') {
@@ -349,16 +361,19 @@ export default {
     },
     submit() {
       const { refForm } = this.$refs
-      if (refForm.validate()) {
-        this.isActionButtonDisabled = true
-        this.$store
-          .dispatch('whitelabel/updateData', this.formValues)
-          .finally(() => (this.isActionButtonDisabled = false))
-      } else {
-        return this.$nextTick(() => {
-          const el = refForm.$el.querySelector('.error--text')
-          scrollToComponent(el)
-        })
+      const { UPDATE } = this.PERMISSIONS
+      if (UPDATE.hasPermission) {
+        if (refForm.validate()) {
+          this.isActionButtonDisabled = true
+          this.$store
+            .dispatch('whitelabel/updateData', this.formValues)
+            .finally(() => (this.isActionButtonDisabled = false))
+        } else {
+          return this.$nextTick(() => {
+            const el = refForm.$el.querySelector('.error--text')
+            scrollToComponent(el)
+          })
+        }
       }
     }
   }
