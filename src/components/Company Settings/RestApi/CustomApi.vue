38de5e39b11562a727dff8873fe9ba6ad406010f
Gurkan Ugurlu <gurkan.ugurlu@keepnetlabs.com>
2021-05-11 12:09:32 +0300

    #13929 finished

--- src/components/Company Settings/RestApi/CustomApi.vue
+++ src/components/Company Settings/RestApi/CustomApi.vue
@@ -232,6 +232,7 @@ export default {
   created() {
     this.storedTableSettings = JSON.parse(localStorage.getItem(TABLE_SETTINGS_KEYS.REST_API))
     this.queryHelper = new QueryHelperForTable(this.$router, this.$route)
+    this.queryHelper.setDefaultValues()
     this.queryHelper.controlRouteQuery()
     const { page, size } = this.queryHelper.returnQueryValues()
     this.setQueryValuesToPayload(this.$route.query)
