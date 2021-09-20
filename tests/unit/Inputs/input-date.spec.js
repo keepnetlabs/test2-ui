bf54c9b7f0b0f724cf2b1860a9f02a24928b44a7
Gurkan Ugurlu <gurkan.ugurlu@keepnetlabs.com>
2021-09-20 12:59:08 +0300

    #18157 finished

---
+++ tests/unit/Inputs/input-date.spec.js
@@ -0,0 +1,35 @@
+import { createLocalVue, mount } from '@vue/test-utils'
+import InputDate from '@/components/Common/Inputs/InputDate'
+describe('Input company component', () => {
+  const localVue = createLocalVue()
+
+  it('Check is rendering', () => {
+    const wrapper = mount(InputDate, {
+      localVue
+    })
+    //expecting component is rendering
+    expect(wrapper.find('.el-date-editor').exists()).toBeTruthy()
+  })
+
+  it('Checking props', async () => {
+    const wrapper = mount(InputDate, {
+      localVue
+    })
+    const input = wrapper.find('input')
+    //checking input type
+    const attributes = input.attributes()
+    expect(attributes.type.includes('text')).toBeTruthy()
+    //checking placeholder
+    expect(attributes.placeholder.includes('Select a date')).toBeTruthy()
+  })
+  it('Changing type', async () => {
+    const wrapper = mount(InputDate, {
+      localVue,
+      propsData: {
+        type: 'datetime'
+      }
+    })
+    //checking is datetime
+    expect(wrapper.find('.el-date-editor--datetime').exists()).toBeTruthy()
+  })
+})
