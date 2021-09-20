6267a4d9de2daf2dce52542df68b045df72b314f
Gurkan Ugurlu <gurkan.ugurlu@keepnetlabs.com>
2021-09-20 10:13:16 +0300

    #18159 finished

---
+++ src/components/TestHelpers/TestInputEmailWrapper.vue
@@ -0,0 +1,32 @@
+<template>
+  <TestFormWrapper>
+    <v-text-field
+      v-model.trim="value"
+      id="test--input-last-name"
+      placeholder="Enter an email address"
+      outlined
+      persistent-hint
+      hint="*Required"
+      autocomplete="off"
+      :rules="rules"
+    />
+  </TestFormWrapper>
+</template>
+
+<script>
+import TestFormWrapper from '@/components/TestHelpers/TestFormWrapper'
+import * as Validations from '@/utils/validations'
+import labels from '@/model/constants/labels'
+import { COMMON_CONSTANTS } from '@/model/constants/commonConstants'
+
+export default {
+  name: 'TestInputEmailWrapper',
+  components: { TestFormWrapper },
+  data() {
+    return {
+      value: '',
+      rules: [...COMMON_CONSTANTS.DEFAULT_EMAIL_RULES]
+    }
+  }
+}
+</script>
