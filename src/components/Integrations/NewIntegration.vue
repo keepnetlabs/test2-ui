dc958c15fef21434f432b800b182c2c4e0967eed
Gurkan Ugurlu <gurkan.ugurlu@keepnetlabs.com>
2021-09-27 13:21:53 +0300

    #18394 finished

--- src/components/Integrations/NewIntegration.vue
+++ src/components/Integrations/NewIntegration.vue
@@ -1343,8 +1343,7 @@ export default {
       const isValidForm = refForm.validate()
       if (isValidForm) {
         this.saveDisable = true
-        //this.testConnection(true)
-        this.saveIntegration()
+        this.testConnection(true)
       } else {
         return this.$nextTick(() => {
           const el = refForm.$el.querySelector('.error--text')
@@ -1432,10 +1431,15 @@ export default {
               proxyResourceId: item.proxyResourceId
             }
           })
+          response['data'].data.apiKeys = response['data'].data.apiKeys.length
+            ? response['data'].data.apiKeys
+            : [{ value: '', status: null, resourceId: null }]
+
           if (this.selectedIntegrationType.name === INTEGRATION_TYPES.IBMXFORCE) {
-            response.data.data.password = response['data'].data['apiCredentials'][0].password
-            response.data.data.proxyResourceId =
-              response['data'].data['apiCredentials'][0].proxyResourceId
+            response.data.data.password = response['data'].data['apiCredentials'].length
+              ? response['data'].data['apiCredentials'][0].password
+              : ''
+            response.data.data.proxyResourceId = response['data'].data.proxyResourceId
           }
         } else if (this.selectedIntegrationType.name === 'FortiNet') {
           const { userName, password, resourceId, proxyResourceId } = response['data'].data[
