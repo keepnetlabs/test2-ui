961fc74ff316fec94726b587c56b8452940c12b2
Gurkan Ugurlu <gurkan.ugurlu@keepnetlabs.com>
2021-11-05 15:42:58 +0300

    #19790 updated

--- src/components/CampaignManagerReport/Summary/CampaignManagerReportSummary.vue
+++ src/components/CampaignManagerReport/Summary/CampaignManagerReportSummary.vue
@@ -2,16 +2,34 @@
   <div id="campaign-manager-report-summary" class="campaign-manager-report-summary">
     <CampaignManagerReportSummaryHeader />
     <CampaignManagerReportSummaryCards />
+    <div class="campaign-manager-report-summary__general-info mt-6">
+      <CampaignManagerReportSummaryCampaignInfo />
+    </div>
   </div>
 </template>
 
 <script>
 import CampaignManagerReportSummaryHeader from '@/components/CampaignManagerReport/Summary/CampaignManagerReportSummaryHeader'
 import CampaignManagerReportSummaryCards from '@/components/CampaignManagerReport/Summary/CampaignManagerReportSummaryCards'
+import CampaignManagerReportSummaryCampaignInfo from '@/components/CampaignManagerReport/Summary/CampaignManagerReportSummaryCampaignInfo'
 export default {
   name: 'CampaignManagerReportSummary',
-  components: { CampaignManagerReportSummaryCards, CampaignManagerReportSummaryHeader }
+  components: {
+    CampaignManagerReportSummaryCampaignInfo,
+    CampaignManagerReportSummaryCards,
+    CampaignManagerReportSummaryHeader
+  }
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.campaign-manager-report-summary {
+  &__general-info {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+  .campaign-manager-summary-card__body-item {
+    justify-content: space-between;
+  }
+}
+</style>
---
+++ src/components/CampaignManagerReport/Summary/CampaignManagerReportSummaryCampaignInfo.vue
@@ -0,0 +1,33 @@
+<template>
+  <CampaignManagerSummaryCard icon="mdi-alert-circle" :title="labels.CampaignInfo" :items="items">
+    <template #NotDelivered="{ props:{ key, val } }">
+      <div class="campaign-manager-summary-card__body-item-key">
+        {{ key.slice(0, 1).toUpperCase() + key.slice(1) }}
+        <v-icon small class="ml-2" color="#F56C6C">mdi-alert-circle</v-icon>
+      </div>
+      <div class="campaign-manager-summary-card__body-item-value">{{ val }}</div>
+    </template>
+  </CampaignManagerSummaryCard>
+</template>
+
+<script>
+import CampaignManagerSummaryCard from '@/components/CampaignManager/Summary/CampaignManagerSummaryCard'
+import labels from '@/model/constants/labels'
+export default {
+  name: 'CampaignManagerReportSummaryCampaignInfo',
+  components: { CampaignManagerSummaryCard },
+  data() {
+    return {
+      labels,
+      items: {
+        'Start Date': '28.05.2021 16:29:00',
+        'End Date': '29.05.2021 16:29:00',
+        'Total Target Users': 145,
+        'Not Delivered': 4
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss"></style>
