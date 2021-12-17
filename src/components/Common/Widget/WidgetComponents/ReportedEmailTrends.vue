0cd6c6833248e9341c68a7480adc6e641286392d
Gurkan Ugurlu <gurkan.ugurlu@keepnetlabs.com>
2021-12-17 10:54:11 +0300

    #21507 updated

--- src/components/Common/Widget/WidgetComponents/ReportedEmailTrends.vue
+++ src/components/Common/Widget/WidgetComponents/ReportedEmailTrends.vue
@@ -41,9 +41,9 @@ import WidgetBody from '@/components/Common/Widget/WidgetBody'
 import WidgetContainer from '@/components/Common/Widget/WidgetContainer'
 import WidgetLoading from '@/components/SkeletonLoading/WidgetLoading'
 import Line from '@/components/Common/Charts/Line'
-import { getNotifiedEmailsTrend } from '@/api/dashboard'
 import { getDataTableFieldLabel } from '@/utils/functions'
 import labels from '@/model/constants/labels'
+import { mapGetters } from 'vuex'
 export default {
   name: 'ReportedEmailTrends',
   components: {
@@ -64,200 +64,185 @@ export default {
       empty: {
         message: 'You do not have any reported emails'
       },
-      isLoading: true,
-      axiosPayload: {
-        pageNumber: 1,
-        pageSize: 100000,
-        orderBy: 'CreateTime',
-        ascending: false,
-        filter: {
-          Condition: 'AND',
-          FilterGroups: [
-            {
-              Condition: 'OR',
-              FilterItems: [],
-              FilterGroups: []
-            }
-          ]
-        }
-      },
       chartOptions: {},
       chartData: {}
     }
   },
-  created() {
-    this.callForGetNotifiedEmails()
-  },
   computed: {
+    ...mapGetters({
+      isLoading: 'widgets/getIsLoading',
+      reportedEmailTrends: 'widgets/getReportedEmailTrendsCard'
+    }),
     getTitle() {
       return labels.ReportedEmailTrends
     }
   },
+  watch: {
+    reportedEmailTrends(data) {
+      this.updateReportedEmailTrends(data)
+    }
+  },
+  created() {
+    this.updateReportedEmailTrends(this.reportedEmailTrends)
+  },
   methods: {
     onEmptyBtnClicked() {},
-    callForGetNotifiedEmails() {
-      getNotifiedEmailsTrend(this.axiosPayload)
-        .then((response) => {
-          const {
-            data: { data }
-          } = response
-          if (data.length) {
-            let minDate = Date.now(),
-              maxDate = null,
-              minEmailCount = 0,
-              maxEmailCount = 0
-            let itemTypes = new Set()
-            const newData = data.map((row) => {
-              let { month, result, emailCount } = row
-              const splittedDate = month.split('-')
-              const timeStampOfDate = new Date(splittedDate[0], splittedDate[1] - 1).getTime()
-              if (timeStampOfDate < minDate) {
-                minDate = timeStampOfDate
-              }
-              if (timeStampOfDate > maxDate) {
-                maxDate = timeStampOfDate
-              }
-              if (emailCount < minEmailCount) {
-                minEmailCount = emailCount
-              }
-              if (emailCount > maxEmailCount) {
-                maxEmailCount = emailCount
-              }
-              itemTypes.add(result)
-              return { x: timeStampOfDate, y: emailCount, result }
-            })
+    updateReportedEmailTrends(data) {
+      if (data.length) {
+        let minDate = Date.now(),
+          maxDate = null,
+          minEmailCount = 0,
+          maxEmailCount = 0
+        let itemTypes = new Set()
+        const newData = data.map((row) => {
+          let { month, result, emailCount } = row
+          const splittedDate = month.split('-')
+          const timeStampOfDate = new Date(splittedDate[0], splittedDate[1] - 1).getTime()
+          if (timeStampOfDate < minDate) {
+            minDate = timeStampOfDate
+          }
+          if (timeStampOfDate > maxDate) {
+            maxDate = timeStampOfDate
+          }
+          if (emailCount < minEmailCount) {
+            minEmailCount = emailCount
+          }
+          if (emailCount > maxEmailCount) {
+            maxEmailCount = emailCount
+          }
+          itemTypes.add(result)
+          return { x: timeStampOfDate, y: emailCount, result }
+        })
 
-            let maxEmailCountLeft = maxEmailCount % 10
-            let remanining = 10 - maxEmailCountLeft
-            maxEmailCount += remanining
-            this.chartOptions = {
-              // Look at this bit
-              plugins: {
-                datalabels: {
-                  formatter: function () {
-                    return ''
-                  },
-                  color: '#575757'
-                }
+        let maxEmailCountLeft = maxEmailCount % 10
+        let remanining = 10 - maxEmailCountLeft
+        maxEmailCount += remanining
+        this.chartOptions = {
+          // Look at this bit
+          plugins: {
+            datalabels: {
+              formatter: function () {
+                return ''
               },
-              tooltips: {
-                callbacks: {
-                  title: (tooltipItem) => {
-                    const newDate = new Date(tooltipItem[0].xLabel)
-                    const months = this.months
-                    return `${months[newDate.getMonth()]} ${newDate.getFullYear()}`
-                  }
-                },
-                backgroundColor: '#6d6d6d',
-                displayColors: false
-              },
-              responsive: true,
-              maintainAspectRatio: false,
-              scales: {
-                xAxes: [
-                  {
-                    display: true,
-                    type: 'time',
-                    time: {
-                      unit: 'month'
-                    },
-                    offset: true,
-                    gridLines: {
-                      display: true,
-                      color: 'rgba(128, 151, 177, 0.3)',
-                      borderDash: [3]
-                    },
-                    ticks: {
-                      fontColor: 'rgba(176, 186, 201)',
-                      lineHeight: 1.58,
-                      min: minDate,
-                      max: maxDate
-                    }
-                  }
-                ],
-                yAxes: [
-                  {
-                    offset: true,
-                    gridLines: {
-                      display: true,
-                      color: 'rgba(128, 151, 177, 0.3)',
-                      borderDash: [3]
-                    },
-                    ticks: {
-                      min: minEmailCount,
-                      max: maxEmailCount,
-                      labelOffset: 0,
-                      beginAtZero: true,
-                      padding: -2,
-                      fontColor: 'rgba(176, 186, 201)',
-                      lineHeight: 1.58
-                    }
-                  }
-                ]
-              },
-
-              legend: {
+              color: '#575757'
+            }
+          },
+          tooltips: {
+            callbacks: {
+              title: (tooltipItem) => {
+                const newDate = new Date(tooltipItem[0].xLabel)
+                const months = this.months
+                return `${months[newDate.getMonth()]} ${newDate.getFullYear()}`
+              }
+            },
+            backgroundColor: '#6d6d6d',
+            displayColors: false
+          },
+          responsive: true,
+          maintainAspectRatio: false,
+          scales: {
+            xAxes: [
+              {
                 display: true,
-                labels: {
-                  usePointStyle: true,
-                  fontColor: '#757575',
-                  generateLabels(chart = {}) {
-                    const { data } = chart
-                    return data.datasets.map((item) => {
-                      return {
-                        text: item.label,
-                        fillStyle: item.borderColor,
-                        lineWidth: 0
-                      }
-                    })
-                  },
-                  fontFamily: 'Open-sans,sans-serif',
-                  padding: 16,
-                  fontSize: 12
+                type: 'time',
+                time: {
+                  unit: 'month'
+                },
+                offset: true,
+                gridLines: {
+                  display: true,
+                  color: 'rgba(128, 151, 177, 0.3)',
+                  borderDash: [3]
+                },
+                ticks: {
+                  fontColor: 'rgba(176, 186, 201)',
+                  lineHeight: 1.58,
+                  min: minDate,
+                  max: maxDate
                 }
               }
-            }
-            itemTypes = [...itemTypes]
-            const datasets = []
-            const colors = {
-              Undetected: {
-                backgroundColor: 'white',
-                borderColor: '#00bcd4',
-                fill: false,
-                pointRadius: 3,
-                borderWidth: 2,
-                lineTension: 0
-              },
-              Phishing: {
-                backgroundColor: 'white',
-                borderColor: '#f56c6c',
-                fill: false,
-                pointRadius: 3,
-                borderWidth: 2,
-                lineTension: 0
-              },
-              Malicious: {
-                backgroundColor: 'white',
-                borderColor: '#b83a3a',
-                fill: false,
-                pointRadius: 3,
-                borderWidth: 2,
-                lineTension: 0
+            ],
+            yAxes: [
+              {
+                offset: true,
+                gridLines: {
+                  display: true,
+                  color: 'rgba(128, 151, 177, 0.3)',
+                  borderDash: [3]
+                },
+                ticks: {
+                  min: minEmailCount,
+                  max: maxEmailCount,
+                  labelOffset: 0,
+                  beginAtZero: true,
+                  padding: -2,
+                  fontColor: 'rgba(176, 186, 201)',
+                  lineHeight: 1.58
+                }
               }
-            }
-            for (let itemType of itemTypes) {
-              datasets.push({
-                label: getDataTableFieldLabel(itemType),
-                ...colors[itemType],
-                data: newData.filter((item) => item.result === itemType)
-              })
-            }
-            this.chartData = {
-              datasets
+            ]
+          },
+
+          legend: {
+            display: true,
+            labels: {
+              usePointStyle: true,
+              fontColor: '#757575',
+              generateLabels(chart = {}) {
+                const { data } = chart
+                return data.datasets.map((item) => {
+                  return {
+                    text: item.label,
+                    fillStyle: item.borderColor,
+                    lineWidth: 0
+                  }
+                })
+              },
+              fontFamily: 'Open-sans,sans-serif',
+              padding: 16,
+              fontSize: 12
             }
           }
-        })
-        .finally(() => (this.isLoading = false))
+        }
+        itemTypes = [...itemTypes]
+        const datasets = []
+        const colors = {
+          Undetected: {
+            backgroundColor: 'white',
+            borderColor: '#00bcd4',
+            fill: false,
+            pointRadius: 3,
+            borderWidth: 2,
+            lineTension: 0
+          },
+          Phishing: {
+            backgroundColor: 'white',
+            borderColor: '#f56c6c',
+            fill: false,
+            pointRadius: 3,
+            borderWidth: 2,
+            lineTension: 0
+          },
+          Malicious: {
+            backgroundColor: 'white',
+            borderColor: '#b83a3a',
+            fill: false,
+            pointRadius: 3,
+            borderWidth: 2,
+            lineTension: 0
+          }
+        }
+        for (let itemType of itemTypes) {
+          datasets.push({
+            label: getDataTableFieldLabel(itemType),
+            ...colors[itemType],
+            data: newData.filter((item) => item.result === itemType)
+          })
+        }
+        this.chartData = {
+          datasets
+        }
+      }
     }
   }
 }
