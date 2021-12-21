15ca7c459122f1b9d2b54bd79d1a5124c1215386
Gurkan Ugurlu <gurkan.ugurlu@keepnetlabs.com>
2021-12-21 14:00:01 +0300

    little fix

--- vue.config.js
+++ vue.config.js
@@ -36,12 +36,5 @@ module.exports = {
         }
       ]
     }
-  },
-  chainWebpack: (config) => {
-    config.plugin('html').tap((args) => {
-      args[0].meta = { charset: 'UTF-8' }
-      console.log('args', args)
-      return args
-    })
   }
 }
