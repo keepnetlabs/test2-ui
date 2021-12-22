9ae80b1fd5fa8a77897ef1b37c2eb3bd34d85c8a
Gurkan Ugurlu <gurkan.ugurlu@keepnetlabs.com>
2021-12-22 16:56:45 +0300

    #21511 finished

--- src/components/PhishingScenarios/NewEmailTemplates.vue
+++ src/components/PhishingScenarios/NewEmailTemplates.vue
@@ -357,8 +357,64 @@ export default {
         ]
       },
       editItemsDisabled: false,
-      methodItems: [],
-      difficultyItems: []
+      methodItems: [
+        {
+          resourceId: 'WNZt0sCVCWB3',
+          genericCodeTypeId: 19,
+          genericCodeTypeName: 'Phishing Simulator Categories',
+          name: 'Click Only',
+          code: '1',
+          description: null,
+          orderNumber: 1
+        },
+        {
+          resourceId: 'DYC0gugxJMjT',
+          genericCodeTypeId: 19,
+          genericCodeTypeName: 'Phishing Simulator Categories',
+          name: 'Data Submission',
+          code: '2',
+          description: null,
+          orderNumber: 2
+        },
+        {
+          resourceId: '7dLrW2kdBTDs',
+          genericCodeTypeId: 19,
+          genericCodeTypeName: 'Phishing Simulator Categories',
+          name: 'Attachment',
+          code: '3',
+          description: null,
+          orderNumber: 3
+        }
+      ],
+      difficultyItems: [
+        {
+          resourceId: 'mT0CeYGgKsVb',
+          genericCodeTypeId: 20,
+          genericCodeTypeName: 'Phishing Simulator Difficulties',
+          name: 'Easy',
+          code: '1',
+          description: null,
+          orderNumber: 1
+        },
+        {
+          resourceId: 'Z5XeVlpw6Dps',
+          genericCodeTypeId: 20,
+          genericCodeTypeName: 'Phishing Simulator Difficulties',
+          name: 'Medium',
+          code: '2',
+          description: null,
+          orderNumber: 2
+        },
+        {
+          resourceId: 'c4LCGEB9MayB',
+          genericCodeTypeId: 20,
+          genericCodeTypeName: 'Phishing Simulator Difficulties',
+          name: 'Hard',
+          code: '3',
+          description: null,
+          orderNumber: 3
+        }
+      ]
     }
   },
   props: {
@@ -619,12 +675,6 @@ export default {
     }
   },
   created() {
-    getLookups('Phishing Simulator Categories').then((response) => {
-      this.methodItems = response.data.data
-    })
-    getLookups('Phishing Simulator Difficulties').then((response) => {
-      this.difficultyItems = response.data.data
-    })
     this.callForMergedTags()
     if (this.isEdit) {
       getEmailTemplatePreviewContent(this.emailTemplateId).then((response) => {
