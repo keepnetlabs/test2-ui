f06a4031acffd2d9c0351a385aee8dd98c5824e3
Gurkan Ugurlu <gurkan.ugurlu@keepnetlabs.com>
2022-01-19 15:35:37 +0300

    #22325 finished

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -64,7 +64,7 @@ export function phone(value, message) {
 export function email(value, message = 'Invalid email address') {
   value = getValue(value)
   return value
-    ? /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/gi.test(
+    ? /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,255}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,255}[a-zA-Z0-9])?)*$/.test(
         value
       ) || message
     : true
