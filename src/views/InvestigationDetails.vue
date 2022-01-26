a62f56d0177fc00b5156b0974cb6c07d3d36717f
Gurkan Ugurlu <gurkan.ugurlu@keepnetlabs.com>
2022-01-26 12:45:16 +0300

    #23352 finished

--- src/views/InvestigationDetails.vue
+++ src/views/InvestigationDetails.vue
@@ -2336,7 +2336,10 @@ export default {
       if (this.isMobileVisible && this.windowWidth < 769) {
         this.isMobileInfo = true
       }
-      this.$router.push(`/investigation-details/${resp.data.data.resourceId}`)
+      if (resp?.data?.data?.resourceId) {
+        this.$router.push(`/investigation-details/${resp.data.data.resourceId}`)
+      }
+
       this.refreshDatatable()
       this.isWantToAddNewCommunity = false
     },
