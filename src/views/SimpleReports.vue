bc30d0867b67ca4e7a6578c37e2e8229a950e0a3
Gurkan Ugurlu <gurkan.ugurlu@keepnetlabs.com>
2022-03-21 13:56:41 +0300

    #24553 started

--- src/views/SimpleReports.vue
+++ src/views/SimpleReports.vue
@@ -7,15 +7,32 @@
 </template>
 
 <script>
+import { useLoading } from '@/hooks/useLoading'
+import { getTicket } from '@/api/common'
 export default {
   name: 'SimpleReports',
+  mixins: [useLoading],
   data() {
     const companyResourceId =
       localStorage.getItem('isSelectCompany') === 'true'
         ? localStorage.getItem('companyRequestId')
         : localStorage.getItem('companyResourceId')
+    // `https://51.89.212.138/single/?appid=39e2ee85-4add-4156-8baa-980f7086a9b9&sheet=5fe1fcbb-0701-49ed-8a24-79ef90f59831&opt=ctxmenu,currsel&select=$::Company.ResourceId,${companyResourceId}`
     return {
-      src: `https://51.89.212.138/single/?appid=39e2ee85-4add-4156-8baa-980f7086a9b9&sheet=5fe1fcbb-0701-49ed-8a24-79ef90f59831&opt=ctxmenu,currsel&select=$::Company.ResourceId,${companyResourceId}`
+      src: ``
+    }
+  },
+  created() {
+    this.callForData()
+  },
+  methods: {
+    callForData() {
+      this.setLoading(true)
+      getTicket()
+        .then(() => {
+          debugger
+        })
+        .finally(this.setLoading)
     }
   }
 }
