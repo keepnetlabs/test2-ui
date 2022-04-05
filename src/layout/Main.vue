a9124b12afd586f3d746484390ed194c27730264
Gurkan Ugurlu <gurkan.ugurlu@keepnetlabs.com>
2022-04-05 11:59:51 +0300

    #24610 new updates

--- src/layout/Main.vue
+++ src/layout/Main.vue
@@ -460,7 +460,6 @@
                 />
               </v-list-item-content>
             </v-list-item>
-            <!--
             <v-list-item style="padding-left: 0 !important; margin-left: -5px;">
               <v-list-item-content class="menu-item-content">
                 <app-router-link
@@ -473,7 +472,6 @@
                 />
               </v-list-item-content>
             </v-list-item>
-            <!-->
           </v-list-group>
           <v-list-group
             v-if="checkCompanyPermissions()"
