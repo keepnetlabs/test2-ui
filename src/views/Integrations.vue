f6c4b328ec5db2f7852711fee426d93e0cb6c73f
Gurkan Ugurlu <gurkan.ugurlu@keepnetlabs.com>
2022-05-12 09:50:31 +0300

    #25341 siem integrations carried under company settings

--- src/views/Integrations.vue
+++ src/views/Integrations.vue
@@ -8,21 +8,6 @@
               <integrations v-if="tab === 'integrations'" ref="refIntegrations"></integrations>
             </el-tab-pane>
             <el-tab-pane
-              v-if="
-                PERMISSIONS.SIEM_PERMISSIONS.SEARCH &&
-                PERMISSIONS.SIEM_PERMISSIONS.SEARCH.hasPermission
-              "
-              name="siem-integrations"
-              :label="labels.SIEMIntegrations"
-              :id="`${labels.SIEMIntegrations.toLowerCase()}-content`"
-            >
-              <s-i-e-m-integrations
-                v-if="tab === 'siem-integrations'"
-                ref="refSIEMIntegrations"
-                :PERMISSIONS="PERMISSIONS.SIEM_PERMISSIONS"
-              ></s-i-e-m-integrations>
-            </el-tab-pane>
-            <el-tab-pane
               name="advanced-settings"
               :label="labels.AdvancedSettings"
               :id="`${labels.AdvancedSettings.toLowerCase()}-content`"
@@ -43,35 +28,21 @@
 import Integrations from '../components/Integrations/Integrations'
 import labels from '@/model/constants/labels'
 import AdvancedSettings from '@/components/Integrations/AdvancedSettings/AdvancedSettings'
-import SIEMIntegrations from '@/components/Integrations/SIEMIntegrations/SIEMIntegrations'
-import PERMISSIONS from '@/permissions'
-import { getPermissionsOfAllItems } from '@/utils/functions'
 export default {
   name: 'Integrations',
   components: {
-    SIEMIntegrations,
     integrations: Integrations,
     'advanced-settings': AdvancedSettings
   },
   data() {
     return {
       tab: 'integrations',
-      labels,
-      PERMISSIONS: {
-        SIEM_PERMISSIONS: {}
-      }
+      labels
     }
-  },
-  created() {
-    this.getPermissions()
   },
   methods: {
     changeTabStatus(tabStatus) {
       this.tab = tabStatus
-    },
-    getPermissions() {
-      const { SIEM_INTEGRATION } = PERMISSIONS
-      this.$set(this.PERMISSIONS, 'SIEM_PERMISSIONS', getPermissionsOfAllItems(SIEM_INTEGRATION))
     }
   },
   beforeRouteLeave(to, from, next) {
