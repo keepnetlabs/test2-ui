7c1f7fc800dadddeddc3e9afdd9447efa703444d
Yağızhan Avcı <yagizhan.avci@keepnetlabs.com>
2022-06-02 17:26:16 +0300

    fixed typo

--- src/components/CampaignManager/CampaignManagerPreview.vue
+++ src/components/CampaignManager/CampaignManagerPreview.vue
@@ -125,9 +125,9 @@ export default {
           const { landingPageTemplate: landingPage } = phishingScenarioPreviewDto
           this.emailTemplate = phishingScenarioPreviewDto?.emailTemplate?.template || ''
           this.emailTemplateParams = {
-            name: phishingScenarioPreviewDto?.emailTemplate?.name,
-            fromName: phishingScenarioPreviewDto?.emailTemplate?.fromName,
-            fromAddress: phishingScenarioPreviewDto?.emailTemplate?.fromAddress
+            name: phishingScenarioPreviewDto?.emailTemplate?.name || '',
+            fromName: phishingScenarioPreviewDto?.emailTemplate?.fromName || '',
+            fromAddress: phishingScenarioPreviewDto?.emailTemplate?.fromAddress || ''
           }
           this.landingPageTemplate = landingPage.landingPages[0].content
           this.landingPageParams = {
