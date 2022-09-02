3e59348480867a07d94b2d205f3005f99c2a2973
Mehmet Murat Bozkır <58207567+mehmetmuratbozkir@users.noreply.github.com>
2022-09-02 18:54:37 +0300

    Add api request header 'X-IR-API-KEY' for Ets endpoints

--- src/api/emailThreatSimlator.js
+++ src/api/emailThreatSimlator.js
@@ -2,75 +2,155 @@ import emailThreatSimlatorRequest from '@/utils/emailThreatSimulatorRequest'
 import testRequest from '@/utils/testRequest'
 
 export function getQuickScanList(payload) {
-  return emailThreatSimlatorRequest.post(`/quick-scan/search`, payload)
+  return emailThreatSimlatorRequest.post(`/quick-scan/search`, payload, {
+    headers: {
+      'X-IR-API-KEY': APP_CONFIG.VUE_APP_API_KEY,
+      'X-IR-COMPANY-ID': localStorage.getItem('companyRequestId')
+    }
+  })
 }
 export function deleteQuickScanItem(resourceId) {
-  return emailThreatSimlatorRequest.delete(`/quick-scan/${resourceId}`)
+  return emailThreatSimlatorRequest.delete(`/quick-scan/${resourceId}`, {
+    headers: {
+      'X-IR-API-KEY': APP_CONFIG.VUE_APP_API_KEY,
+      'X-IR-COMPANY-ID': localStorage.getItem('companyRequestId')
+    }
+  })
 }
 export function getValidateContinuousScan(payload) {
-  return emailThreatSimlatorRequest.post(`/quick-scan/validate-continuous-scan`, payload)
+  return emailThreatSimlatorRequest.post(`/quick-scan/validate-continuous-scan`, payload, {
+    headers: {
+      'X-IR-API-KEY': APP_CONFIG.VUE_APP_API_KEY,
+      'X-IR-COMPANY-ID': localStorage.getItem('companyRequestId')
+    }
+  })
 }
 export function getQuickScanCreate(payload) {
-  return emailThreatSimlatorRequest.post(`/quick-scan`, payload)
+  return emailThreatSimlatorRequest.post(`/quick-scan`, payload, {
+    headers: {
+      'X-IR-API-KEY': APP_CONFIG.VUE_APP_API_KEY,
+      'X-IR-COMPANY-ID': localStorage.getItem('companyRequestId')
+    }
+  })
 }
 export function getQuickScanById(resourceId) {
-  return emailThreatSimlatorRequest.get(`/quick-scan/${resourceId}`)
+  return emailThreatSimlatorRequest.get(`/quick-scan/${resourceId}`, {
+    headers: {
+      'X-IR-API-KEY': APP_CONFIG.VUE_APP_API_KEY,
+      'X-IR-COMPANY-ID': localStorage.getItem('companyRequestId')
+    }
+  })
 }
 export function getQuickScanCount() {
-  return emailThreatSimlatorRequest.get(`/quick-scan/continuous-scan-count`)
+  return emailThreatSimlatorRequest.get(`/quick-scan/continuous-scan-count`, {
+    headers: {
+      'X-IR-API-KEY': APP_CONFIG.VUE_APP_API_KEY,
+      'X-IR-COMPANY-ID': localStorage.getItem('companyRequestId')
+    }
+  })
 }
 export function exportQuickScan(payload) {
   return emailThreatSimlatorRequest.post(`/quick-scan/search/export`, payload, {
-    responseType: 'blob'
+    responseType: 'blob',
+    'X-IR-API-KEY': APP_CONFIG.VUE_APP_API_KEY,
+    'X-IR-COMPANY-ID': localStorage.getItem('companyRequestId')
   })
 }
 export function getAttackVectorList(payload) {
-  return emailThreatSimlatorRequest.post(`/plugin/search`, payload)
+  return emailThreatSimlatorRequest.post(`/plugin/search`, payload, {
+    headers: {
+      'X-IR-API-KEY': APP_CONFIG.VUE_APP_API_KEY,
+      'X-IR-COMPANY-ID': localStorage.getItem('companyRequestId')
+    }
+  })
 }
 export function deleteAttackVectorItem(resourceId) {
-  return emailThreatSimlatorRequest.delete(`/plugin/${resourceId}`)
+  return emailThreatSimlatorRequest.delete(`/plugin/${resourceId}`, {
+    headers: {
+      'X-IR-API-KEY': APP_CONFIG.VUE_APP_API_KEY,
+      'X-IR-COMPANY-ID': localStorage.getItem('companyRequestId')
+    }
+  })
 }
 export function getAttackVectorCreate(payload) {
   return emailThreatSimlatorRequest.post(`/plugin`, payload, {
     headers: {
-      'Content-Type': 'multipart/form-data'
+      'Content-Type': 'multipart/form-data',
+      'X-IR-API-KEY': APP_CONFIG.VUE_APP_API_KEY,
+      'X-IR-COMPANY-ID': localStorage.getItem('companyRequestId')
     }
   })
 }
 export function getAttackVectorUpdate(payload, resourceId) {
   return emailThreatSimlatorRequest.put(`/plugin/${resourceId}`, payload, {
     headers: {
-      'Content-Type': 'multipart/form-data'
+      'Content-Type': 'multipart/form-data',
+      'X-IR-API-KEY': APP_CONFIG.VUE_APP_API_KEY,
+      'X-IR-COMPANY-ID': localStorage.getItem('companyRequestId')
     }
   })
 }
 export function getAttackVectorById(resourceId) {
-  return emailThreatSimlatorRequest.get(`/plugin/${resourceId}`)
+  return emailThreatSimlatorRequest.get(`/plugin/${resourceId}`, {
+    headers: {
+      'X-IR-API-KEY': APP_CONFIG.VUE_APP_API_KEY,
+      'X-IR-COMPANY-ID': localStorage.getItem('companyRequestId')
+    }
+  })
 }
 export function disableAttackVector(payload) {
-  return emailThreatSimlatorRequest.put(`/plugin/passive`, payload)
+  return emailThreatSimlatorRequest.put(`/plugin/passive`, payload, {
+    headers: {
+      'X-IR-API-KEY': APP_CONFIG.VUE_APP_API_KEY,
+      'X-IR-COMPANY-ID': localStorage.getItem('companyRequestId')
+    }
+  })
 }
 export function enableAttackVector(payload) {
-  return emailThreatSimlatorRequest.put(`/plugin/active`, payload)
+  return emailThreatSimlatorRequest.put(`/plugin/active`, payload, {
+    headers: {
+      'X-IR-API-KEY': APP_CONFIG.VUE_APP_API_KEY,
+      'X-IR-COMPANY-ID': localStorage.getItem('companyRequestId')
+    }
+  })
 }
 export function exportAttacksVector(payload) {
   return emailThreatSimlatorRequest.post(`/plugin/search/export`, payload, {
-    responseType: 'blob'
+    responseType: 'blob',
+    'X-IR-API-KEY': APP_CONFIG.VUE_APP_API_KEY,
+    'X-IR-COMPANY-ID': localStorage.getItem('companyRequestId')
   })
 }
 export function getQuickScanReportCountById(resourceId) {
-  return emailThreatSimlatorRequest.get(`/quick-scan-report/counts-and-score/${resourceId}`)
+  return emailThreatSimlatorRequest.get(`/quick-scan-report/counts-and-score/${resourceId}`, {
+    headers: {
+      'X-IR-API-KEY': APP_CONFIG.VUE_APP_API_KEY,
+      'X-IR-COMPANY-ID': localStorage.getItem('companyRequestId')
+    }
+  })
 }
 export function getQuickScanReportStatsById(resourceId) {
-  return emailThreatSimlatorRequest.get(`/quick-scan-report/stats/${resourceId}`)
+  return emailThreatSimlatorRequest.get(`/quick-scan-report/stats/${resourceId}`, {
+    headers: {
+      'X-IR-API-KEY': APP_CONFIG.VUE_APP_API_KEY,
+      'X-IR-COMPANY-ID': localStorage.getItem('companyRequestId')
+    }
+  })
 }
 
 export function getQuickScanReportList(payload, resourceId) {
-  return emailThreatSimlatorRequest.post(`/quick-scan-item/${resourceId}/search`, payload)
+  return emailThreatSimlatorRequest.post(`/quick-scan-item/${resourceId}/search`, payload, {
+    headers: {
+      'X-IR-API-KEY': APP_CONFIG.VUE_APP_API_KEY,
+      'X-IR-COMPANY-ID': localStorage.getItem('companyRequestId')
+    }
+  })
 }
 export function exportQuickScanReportList(payload, resourceId) {
   return emailThreatSimlatorRequest.post(`/quick-scan-item/${resourceId}/search/export`, payload, {
-    responseType: 'blob'
+    responseType: 'blob',
+    'X-IR-API-KEY': APP_CONFIG.VUE_APP_API_KEY,
+    'X-IR-COMPANY-ID': localStorage.getItem('companyRequestId')
   })
 }
 export function getLookupNameList() {
