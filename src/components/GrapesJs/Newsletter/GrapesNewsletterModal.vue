714de157cb40a45f04e3aed1fc0c47b1e36b3e77
Yağızhan Avcı <yagizhan.avci@keepnetlabs.com>
2022-09-06 18:42:58 +0300

    developments reverted

--- src/components/GrapesJs/Newsletter/GrapesNewsletterModal.vue
+++ src/components/GrapesJs/Newsletter/GrapesNewsletterModal.vue
@@ -645,62 +645,10 @@ export default {
           block.attributes.category = {
             label: 'Basic'
           }
-          block.attributes.content = `
-        <table
-          border="0"
-          cellpadding="0"
-          cellspacing="0"
-          role="presentation"
-          style="border-collapse: separate; line-height: 100%"
-        >
-          <tbody>
-            <tr>
-              <td class="aligncenter content-block">
-                <div>
-                  <!--[if mso]>
-                    <v:roundrect
-                      xmlns:v="urn:schemas-microsoft-com:vml"
-                      xmlns:w="urn:schemas-microsoft-com:office:word"
-                      style=" v-text-anchor: middle"
-                      arcsize="17%"
-                      strokecolor="#2196F3"
-                      fillcolor="#2196F3"
-                    >
-                      <w:anchorlock />
-                      <center
-                        style="
-                          color: #ffffff;
-                          font-family: Helvetica;
-                          font-size: 16px;
-                          font-weight: 400;
-                        "
-                      >
-                        Button
-                      </center>
-                    </v:roundrect>
-                  <![endif]-->
-                  <a
-                    style="
-                        background-color: #2196F3;
-                        padding: 8px 12px;
-                        border-radius: 4px;
-                        display: inline-block;
-                      color: #ffffff;
-                      font-family: Helvetica, sans-serif;
-                      font-size: 16px;
-                      font-weight: 400;
-                      text-align: center;
-                      text-decoration: none;
-                      -webkit-text-size-adjust: none;
-                      mso-hide: all;
-                    "
-                    >Button
-                  </a>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>`
+          block.attributes.content = block.attributes.content.replace(
+            'button',
+            'button grapes-custom-button'
+          )
         } else if (block.attributes.id === 'divider') {
           block.attributes.category = {
             label: 'Layout'
