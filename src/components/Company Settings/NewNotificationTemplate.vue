9015dbe0777b2d29081a9349fcab657e4194403b
Yağızhan Avcı <yagizhan.avci@keepnetlabs.com>
2022-10-17 17:48:00 +0300

    #27996 reverted

--- src/components/Company Settings/NewNotificationTemplate.vue
+++ src/components/Company Settings/NewNotificationTemplate.vue
@@ -559,9 +559,9 @@ export default {
     },
     handleCategoryChange(resourceId = '') {
       const categoryIndex = this.categoryItems.findIndex((item) => item.value === resourceId)
-      console.log('categoryIndex',categoryIndex)
-      console.log('item',this.categoryItems[categoryIndex])
-      this.formValues.template = this.categoryItems[categoryIndex].template
+      if (categoryIndex !== -1) {
+        this.formValues.template = this.categoryItems[categoryIndex].template
+      }
       if (!this.blockManagerComponents.hasOwnProperty(resourceId)) {
         this.callForMergedTags(resourceId)
       } else {
