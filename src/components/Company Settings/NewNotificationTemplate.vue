899971cad2f29eb4d337cc0ab6caf962ba899cad
Yağızhan Avcı <yagizhan.avci@keepnetlabs.com>
2022-10-18 09:58:13 +0300

    #27996 added console.logs

--- src/components/Company Settings/NewNotificationTemplate.vue
+++ src/components/Company Settings/NewNotificationTemplate.vue
@@ -26,7 +26,7 @@
             :disabled="editItemsDisabled"
           />
         </form-group>
-        <form-group title="Template Type" has-hint>
+        <form-group title="Template Type ASLDKAS" has-hint>
           <k-select
             v-bind="commonRules"
             v-model.trim="formValues.emailTemplateCategoryResourceId"
@@ -559,9 +559,10 @@ export default {
     },
     handleCategoryChange(resourceId = '') {
       const categoryIndex = this.categoryItems.findIndex((item) => item.value === resourceId)
-      if (categoryIndex !== -1) {
+      console.log('resourceId',resourceId)
+      console.log('categoryIndex',categoryIndex)
+      console.log('item',this.categoryItems[categoryIndex])
         this.formValues.template = this.categoryItems[categoryIndex].template
-      }
       if (!this.blockManagerComponents.hasOwnProperty(resourceId)) {
         this.callForMergedTags(resourceId)
       } else {
