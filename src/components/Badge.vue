77d6c4749373ca2e8b793641148ff374329c564b
Gurkan Ugurlu <gurkan.ugurlu@keepnetlabs.com>
2022-12-20 09:14:40 +0300

    yarn lint

--- src/components/Badge.vue
+++ src/components/Badge.vue
@@ -5,7 +5,7 @@
         <v-btn
           v-bind="$options.getDynamicProps(props)"
           v-on="on"
-          style="cursor: default"
+          style="cursor: default;"
           :id="props.id"
           :ripple="false"
           :class="[
@@ -14,7 +14,7 @@
             $options.getBadgeSize(props.size, props),
             props.className,
             { 'k-badge--default': !props.outline },
-            { 'k-badge--text-black': props.textBlack },
+            { 'k-badge--text-black': props.textBlack }
           ]"
         >
           {{ props.text }}
@@ -26,7 +26,7 @@
       v-else
       v-bind="$options.getDynamicProps(props)"
       v-on="props.listeners"
-      style="cursor: default"
+      style="cursor: default;"
       :id="props.id"
       :ripple="false"
       :class="[
@@ -35,7 +35,7 @@
         $options.getBadgeSize(props.size, props),
         props.className,
         { 'k-badge--default': !props.outline },
-        { 'k-badge--text-black': props.textBlack },
+        { 'k-badge--text-black': props.textBlack }
       ]"
     >
       <slot name="content">
@@ -48,93 +48,93 @@
 <script>
 export default {
   functional: true,
-  name: "Badge",
+  name: 'Badge',
   props: {
     id: {
-      type: String,
+      type: String
     },
     color: {
       type: String,
-      default: "",
+      default: ''
     },
     isBlackText: {
       type: Boolean,
-      default: false,
+      default: false
     },
     defaultBackgroundColor: {
       type: String,
-      default: "#fff",
+      default: '#fff'
     },
     text: {
-      type: String || Number,
+      type: String || Number
     },
     textBlack: {
-      type: Boolean,
+      type: Boolean
     },
     listeners: {},
     fullWidth: {
       type: Boolean,
-      default: true,
+      default: true
     },
     size: {
       type: String,
-      default: "medium",
+      default: 'medium'
     },
     style: {
-      type: Object,
+      type: Object
     },
     className: {
-      type: String,
+      type: String
     },
     col: {
-      type: Object,
+      type: Object
     },
     outline: {
       type: Boolean,
-      default: true,
+      default: true
     },
     isErrorState: {
-      default: false,
+      default: false
     },
     errorStateValue: {
-      default: "",
-    },
+      default: ''
+    }
   },
   getBadgeSize(val) {
-    let retValue = "";
+    let retValue = ''
 
-    if (val === "medium") {
-      retValue = "k-badge__sizes--medium";
+    if (val === 'medium') {
+      retValue = 'k-badge__sizes--medium'
     }
 
-    if (val === "small") {
-      retValue = "k-badge__sizes--small";
+    if (val === 'small') {
+      retValue = 'k-badge__sizes--small'
     }
 
-    if (val === "mini") {
-      retValue = "k-badge__sizes--mini";
+    if (val === 'mini') {
+      retValue = 'k-badge__sizes--mini'
     }
 
-    if (val === "auto") {
-      retValue = "k-badge__sizes--auto";
+    if (val === 'auto') {
+      retValue = 'k-badge__sizes--auto'
     }
 
-    return retValue;
+    return retValue
   },
   getDynamicProps(props = {}) {
-    const dynamicProps = {};
+    const dynamicProps = {}
     if (!props.outline) {
-      dynamicProps["color"] = props.color;
-      dynamicProps["style"] = props.col && props.col.props && props.col.props.style;
-      dynamicProps["rounded"] = true;
-      return dynamicProps;
+      dynamicProps['color'] = props.color
+      dynamicProps['style'] = props.col && props.col.props && props.col.props.style
+      dynamicProps['rounded'] = true
+      return dynamicProps
     }
-    dynamicProps["color"] = props.defaultBackgroundColor;
-    dynamicProps["style"] = [
+    dynamicProps['color'] = props.defaultBackgroundColor
+    dynamicProps['style'] = [
       { border: `1px solid ${props.color} !important`, color: props.color },
-      props.col && props.col.props && props.col.props.style,
-    ];
-    return dynamicProps;
-  },
-};
+      props.col && props.col.props && props.col.props.style
+    ]
+    return dynamicProps
+  }
+}
 </script>
