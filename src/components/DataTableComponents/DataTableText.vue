ff94c2acdba0e8590d02fe77c866258085baa58c
Yağızhan Avcı <yagizhan.avci@keepnetlabs.com>
2022-12-23 15:39:03 +0300

    #29231, #29529 completed

--- src/components/DataTableComponents/DataTableText.vue
+++ src/components/DataTableComponents/DataTableText.vue
@@ -1,43 +1,38 @@
 <template functional>
-  <div>
-    <span
-      class="dataTableText-validation-error"
-      v-if="props.scope.row && props.scope.row['validationDetail']"
-      >{{
-        props.scope.row['validationDetail'].find((item) => {
-          return item.fieldName.toLowerCase() == props.col.property.toLowerCase() && item.message
-        }) &&
-        props.scope.row['validationDetail'].find((item) => {
-          return item.fieldName.toLowerCase() == props.col.property.toLowerCase() && item.message
-        }).message
-      }}</span
-    >
-    <span
-      v-if="
-        props.scope.row &&
-        (props.scope.row[props.col.property] !== undefined ||
-          props.scope.row[props.col.property] !== null ||
-          props.scope.row[props.col.property] !== '')
-      "
-      :class="{
-        'dataTableText-main-error':
-          props.scope.row &&
-          props.scope.row['validationDetail'] &&
+  <div class="d-flex align-center">
+    <template v-if="$options.getHasRowContent(props)">
+      <span
+        v-if="$options.getHasRowContent(props)"
+        :class="{
+          'dataTableText-main-error': $options.getHasValidationError(props)
+        }"
+        >{{ props.scope.row[props.col.property] }}</span
+      >
+      <v-tooltip v-if="$options.getHasValidationError(props)" bottom>
+        <template #activator="{ on }">
+          <v-icon
+            v-if="$options.getHasValidationError(props)"
+            v-on="on"
+            class="ml-2"
+            color="#B83A3A"
+            medium
+            >mdi-information</v-icon
+          >
+        </template>
+        <span>{{
           props.scope.row['validationDetail'].find((item) => {
             return item.fieldName.toLowerCase() == props.col.property.toLowerCase() && item.message
-          })
-      }"
-      >{{ props.scope.row[props.col.property] }}</span
-    >
+          }) &&
+          props.scope.row['validationDetail'].find((item) => {
+            return item.fieldName.toLowerCase() == props.col.property.toLowerCase() && item.message
+          }).message
+        }}</span>
+      </v-tooltip>
+    </template>
     <span
       v-else
       :class="{
-        'dataTableText-main-error':
-          props.scope.row &&
-          props.scope.row['validationDetail'] &&
-          props.scope.row['validationDetail'].find((item) => {
-            return item.fieldName.toLowerCase() == props.col.property.toLowerCase() && item.message
-          })
+        'dataTableText-main-error': $options.getHasValidationError(props)
       }"
       >{{ props.col['emptyText'] || '' }}</span
     >
@@ -55,6 +50,23 @@ export default {
     col: {
       type: Object
     }
+  },
+  getHasRowContent(props) {
+    return (
+      props.scope.row &&
+      (props.scope.row[props.col.property] !== undefined ||
+        props.scope.row[props.col.property] !== null ||
+        props.scope.row[props.col.property] !== '')
+    )
+  },
+  getHasValidationError(props) {
+    return (
+      props.scope.row &&
+      props.scope.row['validationDetail'] &&
+      props.scope.row['validationDetail'].find((item) => {
+        return item.fieldName.toLowerCase() == props.col.property.toLowerCase() && item.message
+      })
+    )
   }
 }
 </script>
