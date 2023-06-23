90a68b5cae8e66227c7f8a22a4ed34f8d8f962b0
Gurkan Ugurlu <gurkan.ugurlu@keepnetlabs.com>
2023-06-23 10:25:07 +0300

    console.log deleted

--- src/components/VishingReport/VishingReportSummary.vue
+++ src/components/VishingReport/VishingReportSummary.vue
@@ -153,7 +153,6 @@ export default {
       }
     },
     getResendDialogItems() {
-      console.log('this.vishingSummary', this.vishingSummary)
       const {
         answeredCount: answered = 0,
         noResponseCount: noResponse = 0,
