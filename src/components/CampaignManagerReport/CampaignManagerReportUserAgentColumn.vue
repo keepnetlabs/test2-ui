65909ad1595feeed0a9f4ef052a603e6ce23a4a1
Yağızhan Avcı <yagizhan.avci@keepnetlabs.com>
2023-08-16 09:54:28 +0300

    #30760 fixed

--- src/components/CampaignManagerReport/CampaignManagerReportUserAgentColumn.vue
+++ src/components/CampaignManagerReport/CampaignManagerReportUserAgentColumn.vue
@@ -1,10 +1,16 @@
 <template>
   <span>
-    <v-tooltip v-if="isRenderTooltip" bottom max-width="180" nudge-bottom="4" nudge-left="80">
+    <v-tooltip
+      v-if="isRenderTooltip"
+      bottom
+      max-width="180"
+      nudge-bottom="4"
+      nudge-left="80"
+    >
       <template #activator="{ on }">
         <v-icon
           v-on="on"
-          style="margin-top: -3px; font-size: 20px; margin-right: 2px;"
+          style="margin-top: -3px; font-size: 20px; margin-right: 2px"
           color="#B6791D"
           >mdi-information</v-icon
         >
@@ -16,23 +22,25 @@
 </template>
 
 <script>
-import { UNUSUAL_TYPES } from '@/components/CampaignManagerReport/Opened/utils'
+import { UNUSUAL_TYPES } from "@/components/CampaignManagerReport/Opened/utils"
 
 export default {
-  name: 'CampaignManagerReportUserAgentColumn',
+  name: "CampaignManagerReportUserAgentColumn",
   props: {
     scope: {
-      type: Object
-    }
+      type: Object,
+    },
   },
   computed: {
     isRenderTooltip() {
       if (!this.scope?.row?.sandBoxType) return false
-      const binaryArray = this.scope.row.sandBoxType.toString(2).split('').reverse().join('')
-      return [binaryArray[UNUSUAL_TYPES.USER_AGENT], binaryArray[UNUSUAL_TYPES.HONEYPOT]].includes(
-        '1'
-      )
-    }
-  }
+      const binaryArray = this.scope.row.sandBoxType
+        .toString(2)
+        .split("")
+        .reverse()
+        .join("")
+      return [binaryArray[UNUSUAL_TYPES.USER_AGENT]].includes("1")
+    },
+  },
 }
 </script>
