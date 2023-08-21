aa3f4efc7e724efe28450e0d4a25fdf7a53800d2
Yağızhan Avcı <yagizhan.avci@keepnetlabs.com>
2023-08-21 13:35:22 +0300

    #32160 tags fixed

--- src/components/LandingPage/utils.js
+++ src/components/LandingPage/utils.js
@@ -126,10 +126,10 @@ export const MERGED_TEXTS_MAP = {
   '{FROMNAME}': fromName,
   '{LASTNAME}': lastName,
   '{PHISHINGURL}': phishingUrl,
-  '{DATESENT}': dateSent,
-  '{CURRENTDATEPLUS10DAYS}': currentDatePlusTenDays,
-  '{CURRENTDATEMINUS10DAYS}': currentDateMinusTenDays,
-  '{RANDOMNUMBER1DIGIT}': randomNumberOneDigit,
-  '{RANDOMNUMBER2DIGITS}': randomNumberTwoDigits,
-  '{RANDOMNUMBER3DIGITS}': randomNumberThreeDigits
+  '{DATE_SENT}': dateSent,
+  '{CURRENT_DATE_PLUS_10_DAYS}': currentDatePlusTenDays,
+  '{CURRENT_DATE_MINUS_10_DAYS}': currentDateMinusTenDays,
+  '{RANDOM_NUMBER_1_DIGIT}': randomNumberOneDigit,
+  '{RANDOM_NUMBER_2_DIGITS}': randomNumberTwoDigits,
+  '{RANDOM_NUMBER_3_DIGITS}': randomNumberThreeDigits
 }
