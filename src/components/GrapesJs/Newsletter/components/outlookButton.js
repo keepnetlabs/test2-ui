f2206e1079da5f035612e3778c80d21e3c658536
Gurkan Ugurlu <gurkan.ugurlu@keepnetlabs.com>
2023-09-30 23:11:18 +0300

    outlook width adjustments

--- src/components/GrapesJs/Newsletter/components/outlookButton.js
+++ src/components/GrapesJs/Newsletter/components/outlookButton.js
@@ -9,12 +9,12 @@ const outlookButton = {
   content:
     '<span class="outlook-button-span-id">\n' +
     '  <!--[if mso]>\n' +
-    '    <v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="" style="height:34px;v-text-anchor:middle;width:auto;min-width:65px;" arcsize="12%" stroke="f" fillcolor="#2196F3">\n' +
+    '    <v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="" style="height:34px;v-text-anchor:middle;width:65px;min-width:65px;" arcsize="12%" stroke="f" fillcolor="#2196F3">\n' +
     '        <w:anchorlock/>\n' +
     '        <center style="color:#ffffff; font-family:Arial, sans-serif; font-size:13px;">\n' +
     '    <![endif]-->\n' +
     '  <a id="outlook-button-href-id" href=""\n' +
-    '    style="background-color:#2196F3;border-radius:4px;color:#ffffff;display:inline-block;font-family:sans-serif;font-size:13px;font-weight:bold;line-height:34px;text-align:center;text-decoration:none;width:auto;min-width:65px;-webkit-text-size-adjust:none;">\n' +
+    '    style="background-color:#2196F3;border-radius:4px;color:#ffffff;display:inline-block;font-family:sans-serif;font-size:13px;font-weight:bold;line-height:34px;text-align:center;text-decoration:none;min-width:65px;-webkit-text-size-adjust:none;">\n' +
     '    Button\n' +
     '  </a>\n' +
     '  <!--[if mso]>\n' +
