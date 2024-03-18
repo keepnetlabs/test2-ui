a3b38e515e39f0e03f78a25ae96c49e86592a582
Gurkan Ugurlu <gurkan.ugurlu@keepnetlabs.com>
2024-03-18 13:57:28 +0300

    #32687 new components added

---
+++ src/components/Common/Inputs/InputBehaviour.vue
@@ -0,0 +1,47 @@
+<template>
+  <FormGroup :title="labels.Behaviour">
+    <KSelect
+      :value="value"
+      dense
+      outlined
+      multiple
+      persistent-hint
+      small-chips
+      deletable-chips
+      autocomplete="off"
+      item-text="text"
+      item-value="value"
+      placeholder="Select compliance"
+      :items="getBehaviours"
+      @input="$emit('input', $event)"
+    ></KSelect>
+  </FormGroup>
+</template>
+
+<script>
+import FormGroup from '@/components/SmallComponents/FormGroup.vue'
+import KSelect from '@/components/Common/Inputs/KSelect.vue'
+import { mapGetters } from 'vuex'
+import labels from '@/model/constants/labels'
+
+export default {
+  name: 'InputBehaviour',
+  components: { KSelect, FormGroup },
+  props: {
+    value: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      labels
+    }
+  },
+  computed: {
+    ...mapGetters({
+      getBehaviours: 'trainingLibraryHelpers/getBehaviours'
+    })
+  }
+}
+</script>
---
+++ src/components/Common/Inputs/InputCompliance.vue
@@ -0,0 +1,47 @@
+<template>
+  <FormGroup :title="labels.Compliance">
+    <KSelect
+      :value="value"
+      dense
+      outlined
+      multiple
+      persistent-hint
+      small-chips
+      deletable-chips
+      autocomplete="off"
+      item-text="text"
+      item-value="value"
+      placeholder="Select compliance"
+      :items="getCompliances"
+      @input="$emit('input', $event)"
+    ></KSelect>
+  </FormGroup>
+</template>
+
+<script>
+import FormGroup from '@/components/SmallComponents/FormGroup.vue'
+import KSelect from '@/components/Common/Inputs/KSelect.vue'
+import { mapGetters } from 'vuex'
+import labels from '@/model/constants/labels'
+
+export default {
+  name: 'InputCompliance',
+  components: { KSelect, FormGroup },
+  props: {
+    value: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      labels
+    }
+  },
+  computed: {
+    ...mapGetters({
+      getCompliances: 'trainingLibraryHelpers/getCompliances'
+    })
+  }
+}
+</script>
