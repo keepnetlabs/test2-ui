a913883b2b0477be67da4ee484799ebd5c0de2af
Gurkan Ugurlu <gurkan.ugurlu@keepnetlabs.com>
2024-03-27 12:10:46 +0300

    #32687 new merge tags added

---
+++ src/components/GrapesJs/Newsletter/mergedTexts/learningPathDescription.js
@@ -0,0 +1,19 @@
+import mergedTextsLearningPathDescription from '../blocks/mergedTextsBlocks/learningPathDescription'
+
+const learningPathDescription = {
+  label: 'Learning Path Description',
+  category: 'Merge Tags',
+  attributes: {
+    title: 'To',
+    class: 'merged-text',
+    icon: 'fa fa-text',
+    'data-title': 'Learning Path Description'
+  },
+  content: {
+    tagName: 'span',
+    draggable: true,
+    components: mergedTextsLearningPathDescription
+  }
+}
+
+export default learningPathDescription
