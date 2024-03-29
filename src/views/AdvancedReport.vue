263a9e2073befb10837beb8eb2344ca18effc09b
Gurkan Ugurlu <gurkan.ugurlu@keepnetlabs.com>
2024-03-29 16:15:39 +0300

    #32687 advanced report updates

--- src/views/AdvancedReport.vue
+++ src/views/AdvancedReport.vue
@@ -2,13 +2,7 @@
   <KContainer tabless>
     <DatatableLoading v-if="isLoading" :loading="isLoading" />
     <div :style="isLoading ? 'visibility:hidden;max-height:0px' : ''">
-      <PowerBIReportEmbed
-        v-if="renderReport"
-        :key="renderKey"
-        :embed-config="config"
-        css-class-name="advanced-report-power-bi-container"
-        :event-handlers="eventHandlers"
-      />
+      <div class="advanced-report-power-bi-container" ref="reportContainer"></div>
     </div>
   </KContainer>
 </template>
@@ -16,65 +10,44 @@
 <script>
 import KContainer from '@/components/KContainer/KContainer'
 import ReportsService from '@/api/reports'
-import { PowerBIReportEmbed } from 'powerbi-client-vue-js'
+import * as pbi from 'powerbi-client'
 import DatatableLoading from '@/components/SkeletonLoading/WidgetLoading'
 import { useLoading } from '@/hooks/useLoading'
-import { createRandomCryptStringNumber } from '@/utils/functions'
-
 export default {
   name: 'AdvancedReport',
-  components: { DatatableLoading, KContainer, PowerBIReportEmbed },
+  components: { DatatableLoading, KContainer },
   mixins: [useLoading],
-  created() {
+  mounted() {
     this.callForData()
   },
   data() {
     return {
-      renderReport: false,
-      renderKey: `render-key-${createRandomCryptStringNumber()}`,
-      config: {
-        type: 'report',
-        tokenType: 1,
-        id: '',
-        embedUrl: '',
-        accessToken: '',
-        settings: {
-          panes: {
-            filters: {
-              visible: false
-            },
-            pageNavigation: {
-              visible: false
-            }
-          }
-        }
-      },
-      eventHandlers: new Map([
-        [
-          'loaded',
-          () =>
-            setTimeout(() => {
-              this.setLoading()
-            }, 1000)
-        ]
-      ])
+      report: null
     }
   },
   methods: {
     callForData() {
       const { params = {} } = this.$route || {}
       this.setLoading(true)
-      ReportsService.getReportDetail(params.id)
-        .then((response) => {
-          const { data: { data = {} } = {} } = response || {}
-          const { embedReport = {}, embedToken = {} } = data
-          this.config.embedUrl = embedReport.embedUrl
-          this.config.id = embedReport.reportId
-          this.config.accessToken = embedToken.token
-          this.renderKey = `render-key-${createRandomCryptStringNumber()}`
-          this.renderReport = true
+      ReportsService.getReportDetail(params.id).then((response) => {
+        const { data: { data = {} } = {} } = response || {}
+        const { embedReport = {}, embedToken = {} } = data
+        const config = {
+          type: 'report',
+          tokenType: pbi.models.TokenType.Embed,
+          accessToken: embedToken.token,
+          embedUrl: embedReport.embedUrl,
+          permissions: pbi.models.Permissions.All,
+          settings: {
+            filterPaneEnabled: false,
+            navContentPaneEnabled: false
+          }
+        }
+        this.report = window.powerbi.embed(this.$refs.reportContainer, config)
+        this.report.on('loaded', () => {
+          this.setLoading()
         })
-        .catch(this.setLoading)
+      })
     }
   }
 }
