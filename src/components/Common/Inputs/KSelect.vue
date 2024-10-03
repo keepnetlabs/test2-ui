22b2d189607040c461b567b259e0dade8c21fef8
Yağızhan Avcı <yagizhan.avci@keepnetlabs.com>
2024-10-03 17:02:11 +0300

    #37998 completed

--- src/components/Common/Inputs/KSelect.vue
+++ src/components/Common/Inputs/KSelect.vue
@@ -24,6 +24,9 @@
     <template v-slot:append v-if="slots.append">
       <slot name="append" />
     </template>
+    <template v-slot:prepend-item v-if="slots.prependItem">
+      <slot name="prependItem" />
+    </template>
     <template v-slot:progress v-if="slots.progress">
       <slot name="progress" />
     </template>
