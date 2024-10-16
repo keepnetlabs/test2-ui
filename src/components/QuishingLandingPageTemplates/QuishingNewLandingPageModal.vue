e0a85ea422f7759b03213c11ea983d9c0dff4fca
Yağızhan Avcı <yagizhan.avci@keepnetlabs.com>
2024-10-16 17:15:43 +0300

    #37997 text changed

--- src/components/QuishingLandingPageTemplates/QuishingNewLandingPageModal.vue
+++ src/components/QuishingLandingPageTemplates/QuishingNewLandingPageModal.vue
@@ -158,7 +158,7 @@
                       class="mb-10"
                     >
                       <template #label>
-                        Automatically stop bots to prevent false clicks.
+                        Stop bots to prevent false clicks.
                         <VTooltip bottom max-width="260">
                           <template #activator="{ on }">
                             <v-icon v-on="on" class="ml-2" color="#757575">mdi-information</v-icon>
