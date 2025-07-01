981b02d474af6e811e0b7dfd90f01e6e0f4db469
Gurkan Ugurlu <gurkan.ugurlu@keepnetlabs.com>
2025-07-01 16:38:47 +0300

    mock local storage and window props added

--- tests/unit/index.js
+++ tests/unit/index.js
@@ -1,5 +1,66 @@
 require('regenerator-runtime')
 require('jest-canvas-mock')
+
+// Global browser environment mock for Node.js (CI/CD builds)
+if (typeof window === 'undefined') {
+  global.window = {
+    localStorage: {
+      getItem: () => null,
+      setItem: () => {},
+      removeItem: () => {},
+      clear: () => {}
+    },
+    location: {
+      origin: 'http://localhost',
+      href: 'http://localhost',
+      hostname: 'localhost',
+      pathname: '/',
+      search: '',
+      hash: ''
+    },
+    document: {
+      origin: 'http://localhost'
+    }
+  }
+  global.localStorage = global.window.localStorage
+}
+
+// Mock localStorage for JSDOM environment
+const localStorageMock = (() => {
+  let store = {}
+  return {
+    getItem: function (key) {
+      return store[key] || null
+    },
+    setItem: function (key, value) {
+      store[key] = value.toString()
+    },
+    removeItem: function (key) {
+      delete store[key]
+    },
+    clear: function () {
+      store = {}
+    }
+  }
+})()
+
+Object.defineProperty(window, 'localStorage', {
+  value: localStorageMock
+})
+
+// Mock window.location for test environment
+Object.defineProperty(window, 'location', {
+  value: {
+    origin: 'http://localhost',
+    href: 'http://localhost',
+    hostname: 'localhost',
+    pathname: '/',
+    search: '',
+    hash: ''
+  },
+  writable: true
+})
+
 jest.mock('powerbi-client', () => ({
   get: Promise.resolve({})
 }))
